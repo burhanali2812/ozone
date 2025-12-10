d86d205fee7015eca6565d800205ba45c9a73253
burhanali2812 <syedburhanali830@gmail.com>
2025-12-11 02:41:01 +0500

    set orders

--- .next/dev/server/chunks/[root-of-the-server]__e6b7005e._.js
+++ .next/dev/server/chunks/[root-of-the-server]__e6b7005e._.js
@@ -294,7 +294,8 @@ async function PUT(request) {
         }
         return __TURBOPACK__imported__module__$5b$project$5d2f$OZONE$2f$ozone$2d$water$2d$1$2f$node_modules$2f$next$2f$server$2e$js__$5b$app$2d$route$5d$__$28$ecmascript$29$__["NextResponse"].json({
             message: "Order updated successfully",
-            order: updatedOrder
+            order: updatedOrder,
+            success: true
         }, {
             status: 200
         });
