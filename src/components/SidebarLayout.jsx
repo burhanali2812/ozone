7e698056af4890100d11eef2d3ce37cf04349d76
burhanali2812 <syedburhanali830@gmail.com>
2025-12-31 02:19:06 +0500

    update version

--- src/components/SidebarLayout.jsx
+++ src/components/SidebarLayout.jsx
@@ -6,7 +6,7 @@ export default function SidebarLayout({ children }) {
   const pathname = usePathname();
 
 
-  const noSidebarRoutes = ["/", "/auth","/order"];
+  const noSidebarRoutes = ["/", "/auth","/order","/receipt"];
   const shouldShowSidebar = !noSidebarRoutes.includes(pathname);
 
   return (
